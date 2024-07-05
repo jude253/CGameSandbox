8f753bfdff0757ce5112dc3b0573fdb261b15abf
Jude <judecapachietti@gmail.com>
2024-07-04 23:54:12 -0700

    add drawVerticalTrapezoid

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 const int SCREEN_DIVISOR = 10;
 const int RECT_HEIGHT = SCREEN_HEIGHT / SCREEN_DIVISOR;
 const int RECT_WIDTH = SCREEN_WIDTH / SCREEN_DIVISOR;
-SDL_Rect rect;
+
 void prepareScene(void)
 {
     SDL_SetRenderDrawColor(app.renderer, 96, 128, 255, 255);
@@ -14,6 +14,7 @@ void prepareScene(void)
 }
 
 void drawRect(int x, int y) {
+    SDL_Rect rect;
     rect.x = x;
     rect.y = y;
     rect.w = RECT_WIDTH;
@@ -23,7 +24,7 @@ void drawRect(int x, int y) {
 }
 
 void drawBorderRects(void) {
-    for (int i; i < SCREEN_DIVISOR; i++) {
+    for (int i = 0; i < SCREEN_DIVISOR; i++) {
         drawRect(i*RECT_WIDTH, 0);
         drawRect(i*RECT_WIDTH, SCREEN_HEIGHT-RECT_HEIGHT);
         drawRect(0, i*RECT_HEIGHT);
@@ -31,9 +32,41 @@ void drawBorderRects(void) {
     }
 }
 
+void drawVertLine(int x, int y, int h) {
+    SDL_RenderDrawLine(app.renderer, x, y, x, y+h);
+}
+
+/*
+Draw vertically parallel trapezoid from x_lt to x_rt.
+*/
+void drawVerticalTrapezoid(
+    int x_lt, int y_lt, int x_rt, int y_rt,
+    int x_lb, int y_lb, int x_rb, int y_rb
+    ) {
+    int y_t;
+    int y_b;
+    for (int x = x_lt; x < x_rt; x++) {
+        y_t = (int)(((float)(y_rt - y_lt) / (float)(x_rt - x_lt))*(float)(x-x_lt) + y_lt);
+        y_b = (int)(((float)(y_rb - y_lb) / (float)(x_rb - x_lb))*(float)(x-x_lb) + y_lb);
+        SDL_RenderDrawLine(app.renderer, x, y_t, x, y_b);
+    }
+    
+}
+
 void presentScene(void)
 {   
     SDL_SetRenderDrawColor(app.renderer, 96, 0, 0, 255);
     drawBorderRects();
+
+    drawVerticalTrapezoid(
+        0, 0,             RECT_WIDTH, RECT_HEIGHT,
+        0, SCREEN_HEIGHT, RECT_WIDTH, SCREEN_HEIGHT-RECT_HEIGHT
+    );
+    drawVerticalTrapezoid(
+        SCREEN_WIDTH-RECT_WIDTH, RECT_HEIGHT,               SCREEN_WIDTH, 0,
+        SCREEN_WIDTH-RECT_WIDTH, SCREEN_HEIGHT-RECT_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT
+    );
+
+
     SDL_RenderPresent(app.renderer);
 }
